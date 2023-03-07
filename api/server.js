8959f9d8f7e6e2b3c8d06ff77dff8f4fa0733b6c
EmreSahiner <emre@journeyapp.com>
2023-03-07 07:55:15 +0300

    typo fixes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const server = express();
-const sesssion = require('express-session');
+const session = require('express-session');
 
 const authRouter = require('./auth/auth-router');
 const userRouter = require('./users/users-router');
